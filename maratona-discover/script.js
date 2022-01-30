8cc22624bfd250f232f376f9a47285a1a4f69154
AlesandraIsla <alesandramempis@gmail.com>
2022-01-30 12:00:17 -0300

    Refatacao dos códigos e inclusao do total das entradas, saidas e total

--- maratona-discover/script.js
+++ maratona-discover/script.js
@@ -53,14 +53,44 @@ const transactions = [
 
 
 const Transaction = {
+    all: transactions,
+
+    add(transaction) {
+        Transaction.all.push(transaction);
+        App.reload()
+    },
+
     incomes() {
         //Somar as entradas
+        let income = 0;
+        //pegar todas as transacoes
+        // para cada transacao
+        Transaction.all.forEach(transaction => {
+            //se ela for maior que zero 
+            if(transaction.amount > 0) {
+                // somar a uma variável e retornar a variável
+                income += transaction.amount;
+            }
+        })
+        return income;
     },
     expenses() {
         // somar as saídas
+        let expense = 0;
+        // pegar todas as transacoes
+        // para cada transacao
+        Transaction.all.forEach(transaction => {
+            //se ela for menor que zero 
+            if(transaction.amount < 0) {
+                //somar a uma variável e retornar a variável
+                expense += transaction.amount;
+            }
+        })
+        return expense;
     },
     total() {
         // entradas - saidas
+        return Transaction.incomes() + Transaction.expenses();
     }
 }
 
@@ -68,31 +98,49 @@ const Transaction = {
 const DOM = {
     transationsContainer: document.querySelector('#data-table tbody'),
 
-        // innerHTML é para receber algo dentro dele
-        addTransaction(transaction, index) {
-            const tr = document.createElement('tr')
-            tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+    // innerHTML é para receber algo dentro dele
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index;
 
-            DOM.transationsContainer.appendChild(tr)
-        },
-        innerHTMLTransaction(transaction) {
-            // Alterando a class
-            const CSSclass = transaction.amount > 0 ? "income" : 'expense';
+        DOM.transationsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction, index) {
+        // Alterando a class
+        const CSSclass = transaction.amount > 0 ? "income" : 'expense';
 
-            const amount = Utils.formatCurrency(transaction.amount)
+        const amount = Utils.formatCurrency(transaction.amount)
 
-            const html = `
-            <tr>
-                <td class="description">${transaction.description}</td>
-                <td class="${CSSclass}">${amount}</td>
-                <td  class="date">${transaction.date}</td>
-                <td>
-                    <img src="./assets/minus.svg" alt="Remover Transação">
-                </td>
-            </tr>
-            `
-            return html;
-        }
+        const html = `
+        <tr>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td  class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover Transação">
+            </td>
+        </tr>
+        `
+        return html;
+    },
+
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes());
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses());
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total());
+    },
+
+    clearTransactions() {
+        DOM.transationsContainer.innerHTML = "";
+    }
+
 }
 
 //Conversão da quantidade de casas decimais
@@ -111,9 +159,34 @@ const Utils = {
     }
 }
 
-// DOM.addTransaction(transaction[0])
 
-//Para cada elemento o forEach vai executar uma funcionalidade
-transactions.forEach(function(transaction) { 
-    DOM.addTransaction(transaction)
+const App = {
+    init(){
+        //Para cada elemento o forEach vai executar uma funcionalidade
+        Transaction.all.forEach(transaction => { 
+            DOM.addTransaction(transaction)
+        })
+
+        DOM.updateBalance()
+
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+
+}
+
+App.init()
+
+
+Transaction.add({
+    id:5,
+    description: 'Caneta',
+    amount: 500,
+    date: '23/01/2021'
 })
+
+
+
+
